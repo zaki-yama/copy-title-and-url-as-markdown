fd013a916903d9e148f5750421af52ad14f4a586
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2020-06-05 21:50:47 +0900

    feat: retrieve the option value of `format*` and copy as its format

--- src/background/index.ts
+++ src/background/index.ts
@@ -10,11 +10,15 @@ chrome.commands.onCommand.addListener(command => {
   };
 
   chrome.tabs.query(queryInfo, function(tabs) {
-    chrome.storage.local.get({ format: DEFAULT_FORMAT }, async function(
-      options
-    ) {
+    // All commands are like `copy_as_format_*` (*: 1 or 2 or 3)
+    const formatIndex = command.slice(-1);
+    console.log("format: ", formatIndex);
+
+    const dummyFormat = "${title}---------${url}";
+    const key = `format${formatIndex}`;
+    chrome.storage.local.get({ [key]: dummyFormat }, async function(options) {
       const tab = tabs[0];
-      copyToClipboard(options.format, tab);
+      copyToClipboard(options[key], tab);
       // chrome.browserAction.setBadgeText({ text: "bar" });
 
       console.log("done!");
