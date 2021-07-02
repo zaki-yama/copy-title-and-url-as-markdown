975a1fdc0efb2db08f5fa10e0fb3dd957e1f2a0c
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2021-07-03 00:31:56 +0900

    style: prettier .js

--- .versionrc.js
+++ .versionrc.js
@@ -2,15 +2,15 @@ module.exports = {
   bumpFiles: [
     {
       filename: "package.json",
-      type: "json"
+      type: "json",
     },
     {
       filename: "dist/manifest.json",
-      type: "json"
+      type: "json",
     },
     {
       filename: "package-lock.json",
-      type: "json"
-    }
-  ]
+      type: "json",
+    },
+  ],
 };
