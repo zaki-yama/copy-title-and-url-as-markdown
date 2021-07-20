e81bd2d6d34144f0fb31ca596fb0f81898e68a24
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2021-07-20 20:58:10 +0900

    style: prettier

--- src/background/index.ts
+++ src/background/index.ts
@@ -2,21 +2,21 @@ import { DEFAULT_FORMAT } from "./../constant";
 import { escapeBrackets, copyToClipboard } from "../util";
 import { OptionsType } from "../options/Options";
 
-chrome.commands.onCommand.addListener(command => {
+chrome.commands.onCommand.addListener((command) => {
   console.log("Command:", command);
 
   const queryInfo = {
     active: true,
-    currentWindow: true
+    currentWindow: true,
   };
 
-  chrome.tabs.query(queryInfo, function(tabs) {
+  chrome.tabs.query(queryInfo, function (tabs) {
     // All commands are like `copy_as_format_*` (*: 1 or 2 or 3)
     const formatIndex = command.slice(-1);
     console.log("format: ", formatIndex);
 
     const key = `optionalFormat${formatIndex}`;
-    chrome.storage.local.get(null, function(options: OptionsType) {
+    chrome.storage.local.get(null, function (options: OptionsType) {
       const tab = tabs[0];
 
       copyToClipboard(options[key], tab);
