9ffa9425eafb551bab901faed51d167cdb7aa24e
crosstyan <cf595833532@gmail.com>
2021-08-15 17:20:36 +0800

    Revert "rmmove storybook and dist"
    This reverts commit e89d863200f65202d37dd8964e78d627fd0832ba.

---
+++ .storybook/main.js
@@ -0,0 +1,13 @@
+module.exports = {
+  stories: ["../src/**/*.stories.tsx"],
+  typescript: {
+    check: false,
+    checkOptions: {},
+    reactDocgen: "react-docgen-typescript",
+    reactDocgenTypescriptOptions: {
+      shouldExtractLiteralValuesFromEnum: true,
+      propFilter: (prop) =>
+        prop.parent ? !/node_modules/.test(prop.parent.fileName) : true,
+    },
+  },
+};
