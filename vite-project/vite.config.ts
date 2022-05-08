4e16a27dba71eca1a494e4927b66bb6f3f7de2af
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2022-05-08 21:39:49 +0900

    build: setup @crxjs/vite-plugin

--- vite-project/vite.config.ts
+++ vite-project/vite.config.ts
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import { crx } from "@crxjs/vite-plugin";
+import react from "@vitejs/plugin-react";
+
+import manifest from "./manifest.json";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [react(), crx({ manifest })],
+});
