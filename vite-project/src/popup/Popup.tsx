f0d64402cebb9bf8411725e9453c74dc0adf3f4d
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2022-06-08 03:52:22 +0900

    refactor: popup

---
+++ vite-project/src/popup/Popup.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { Icon, Grid, Row } from "react-lightning-design-system";
+
+type Props = {
+  title: string;
+  url: string;
+};
+
+export const Popup: React.FC<Props> = ({ title, url }) => {
+  console.log("foo");
+  return (
+    <Grid className="slds-gutters">
+      <Row cols={1} align="center">
+        <Icon category="action" icon="approval" container="circle" />
+      </Row>
+      <Row cols={1}>
+        <div className="slds-text-heading_small">{title}</div>
+      </Row>
+      <Row cols={1}>
+        <div
+          className="slds-text-body_small"
+          style={{ wordBreak: "break-all" }}
+        >
+          {url}
+        </div>
+      </Row>
+    </Grid>
+  );
+};
