a73d93b126cb6a885db544ca34652bb75ccea290
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2022-06-14 01:44:54 +0900

    feat: background

---
+++ vite-project/src/background.ts
@@ -0,0 +1,41 @@
+import { DEFAULT_FORMAT } from "./constant";
+import { escapeBrackets, copyToClipboard } from "./util";
+import { OptionsType } from "./options/Options";
+
+chrome.commands.onCommand.addListener((command) => {
+  console.log("Command:", command);
+
+  const queryInfo = {
+    active: true,
+    currentWindow: true,
+  };
+
+  chrome.tabs.query(queryInfo, function (tabs) {
+    // All commands are like `copy_as_format_*` (*: 1 or 2 or 3)
+    const formatIndex = command.slice(-1);
+    console.log("format: ", formatIndex);
+
+    const key = `optionalFormat${formatIndex}`;
+    chrome.storage.local.get(null, function (options: OptionsType) {
+      const tab = tabs[0];
+      const title = tab.title || "";
+      const url = tab.url || "";
+      const tabId = tab.id || 0;
+      console.log(tab.url, tab.title);
+      console.log(options);
+
+      chrome.scripting.executeScript({
+        target: { tabId },
+        func: copyToClipboard,
+        args: [options[key], title, escapeBrackets(url)],
+      });
+
+      chrome.action.setBadgeText({ text: formatIndex });
+      setTimeout(() => {
+        chrome.action.setBadgeText({ text: "" });
+      }, 1000);
+
+      console.log("done!");
+    });
+  });
+});
